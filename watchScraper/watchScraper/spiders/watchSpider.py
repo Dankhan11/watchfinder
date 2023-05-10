a3fa8e17b7770c4d3cd709b0641c0a72baf7d08a
Dankhan11 <64104361+Dankhan11@users.noreply.github.com>
2023-05-10 17:37:14 +0100

    commit

--- watchScraper/watchScraper/spiders/watchSpider.py
+++ watchScraper/watchScraper/spiders/watchSpider.py
@@ -2,7 +2,7 @@ import scrapy
 
 class Spider(scrapy.Spider):
     name = 'watchFinder'
-    start_urls = ['https://www.goldsmiths.co.uk/c/Watches/Mens-Watches']
+    start_urls = ['https://www.goldsmiths.co.uk/c/Watches/Mens-Watches?q=%3Arelevance&page=1&sort=relevance']
 
     def parse(self, response):
         for product in response.css('div.productTile'):
@@ -19,9 +19,8 @@ class Spider(scrapy.Spider):
                     'link': product.css('a').attrib['href'],
                 }
 
-        load = response.css('div.pagination-LoadMorePrevBlock.pagination-LoadMorePrevBlock-next')
-        next_page_number = int(load.css('#pagination-LoadMore::attr(data-next-page)').get())
-        while next_page_number:
-            next_page_url = f'https://www.goldsmiths.co.uk/c/Watches/Mens-Watches?page={next_page_number}'
+        query_params = response.url.split('?')[-1]  # Extract query parameters
+        next_page_number = int(response.css('#pagination-LoadMore::attr(data-next-page)').get())
+        if next_page_number:
+            next_page_url = f'https://www.goldsmiths.co.uk/c/Watches/Mens-Watches?{query_params}&page={next_page_number}'
             yield scrapy.Request(next_page_url, callback=self.parse)
-            next_page_number = int(load.css('#pagination-LoadMore::attr(data-next-page)').get())
